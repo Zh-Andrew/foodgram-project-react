42d812001790755190d47547c5155daf3eceded1
Андрей Жирихин <zhirikhin.andrew@gmail.com>
2022-12-21 22:42:26 +0300

    add workflows

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 dotenv_path = os.path.abspath(
-    os.path.join(os.path.dirname(BASE_DIR), 'infra', '.env')
+    os.path.join(os.path.dirname(BASE_DIR), '.env')
 )
 
 load_dotenv(dotenv_path)
