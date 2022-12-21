1e3ccb7a7cc3da3e70d122ee78257640da5394aa
Андрей Жирихин <zhirikhin.andrew@gmail.com>
2022-12-21 23:59:55 +0300

    flake8, isort changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,8 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
