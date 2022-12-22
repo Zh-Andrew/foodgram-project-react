8dd946869ca362a7e95876cc60d4445d00da8b9f
Андрей Жирихин <zhirikhin.andrew@gmail.com>
2022-12-22 15:33:54 +0300

    before review

--- backend/api/validators.py
+++ backend/api/validators.py
@@ -4,6 +4,6 @@ from django.core.exceptions import ValidationError
 def username_validation(value):
     if value.lower() == 'me':
         raise ValidationError(
-            '"me" запрещено использовать как логин пользователя'
+            '"me" запрещено использовать как логин'
         )
     return value
